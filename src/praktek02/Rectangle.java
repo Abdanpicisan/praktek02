b89b8de8da9c619660d77eb6969b88818afd2801
abdanpicisan <32365705+abdanpicisan@users.noreply.github.com>
2018-04-03 18:37:01 +0800

    constuctor - sama dengan nama class

--- src/praktek02/Rectangle.java
+++ src/praktek02/Rectangle.java
@@ -1,4 +1,7 @@
 package praktek02;
+
+import java.util.logging.Logger;
+
 public class Rectangle {
      double panjang;
     double lebar;
@@ -9,6 +12,12 @@ public class Rectangle {
         System.out.println("Lebar   :"+lebar);
         System.out.println("==================");
     }
+
+    public Rectangle() {
+         panjang=5;
+        lebar=2;
+    }
+ 
     
     double hitungluas(){
         double luas;
